a66f13b31364172e216856af8d5134dab7969c7c
dev-leticia-costa <leticiaccmarques@hotmail.com>
2023-03-08 12:57:12 -0300

    Refactor typeMovement

--- src/main/kotlin/br/com/g6/orgfinanceiro/services/MovementService.kt
+++ src/main/kotlin/br/com/g6/orgfinanceiro/services/MovementService.kt
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-//@RequiredArgsConstructor
 class MovementService {
     @Autowired
     lateinit var movementRepository: MovementRepository
@@ -16,64 +15,14 @@ class MovementService {
     @Autowired
     lateinit var currentUserService: CurrentUserService
 
-    /*
-    //~~método para retornar qual usuário está logado ~~
-    private fun getCurrentUser(): Optional<Users?>? {
-        val userDetails: UserDetails =
-            SecurityContextHolder.getContext().getAuthentication().getPrincipal() as UserDetails
-        return userRepository.findByUsername(userDetails.getUsername())
-    }
 
-    //~~método para salvar movimentações~~
-    fun save(createMovement: Movement): Movement? {
-        return try {
-            val user: Optional<Users> = getCurrentUser()
-            if (user.isPresent()) {
-                createMovement.setUser(user.get())
-                return movementRepository.save(createMovement)
-            }
-            null
-            //createMovement.setUser(userDetails );
-        } catch (e: Exception) {
-            throw RuntimeException("Erro ao salvar movimento" + e.message)
-        }
-    }
-
-    //~~método para os filtros~~
-    fun findByFilter(filter: MovementDto): List<Movement>? {
-        return try {
-            val user: Optional<Users> = getCurrentUser()
-            if (user.isPresent()) {
-                val filterMovement = FilterMovement()
-                filter.setIdUsuario(user.get().getId())
-                return movementRepository.findAll(filterMovement.toSpecification(filter))
-            }
-            null
-        } catch (e: Exception) {
-            throw RuntimeException("Erro na consulta" + e.message)
-        }
-    }
-
-    //~~deletar movimento pelo id~~
-    fun deleteById(idMovement: Long) {
-        try {
-            val user: Optional<Users> = getCurrentUser()
-            if (user.isPresent()) {
-                movementRepository.deleteById(idMovement)
-            }
-        } catch (e: Exception) {
-            throw RuntimeException("Movimento não encontrado " + idMovement + ": " + e.message)
-        }
-    }
-    */
-    //~~método para pegar o saldo~~
     fun getBalance(): BalanceDTO {
         val response = BalanceDTO()
         var user = currentUserService.getCurrentUser()
 
         // Credit
         val filter = MovementDTO()
-        filter.typeMovement = TypeMovement.RECEITA.toString()
+        filter.typeMovement = "1"
         filter.idUser = user?.id
 
         var filterMovement = FilterMovementSpecification(filter)
@@ -86,7 +35,7 @@ class MovementService {
         response.setCredit(totalCredit)
 
         // Debt
-        filter.typeMovement = TypeMovement.DESPESA.toString()
+        filter.typeMovement = "2"
         filter.idUser = user?.id
 
         filterMovement = FilterMovementSpecification(filter)
